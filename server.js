410e5d704370f665bc8c304fef4574911fa5da32
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-08-16 10:58:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = require("./db")
 
 const app = express();
 app.use(cors({
-    origin:["https://lavazzaplatform16082024.vercel.app/"],
+    origin:["https://lavazzaplatform16082024.vercel.app"],
     methods:["POST","GET"],
     credentials:true}));
 
